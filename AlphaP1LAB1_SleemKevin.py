afc6aaa53541ff5f7455d5911e0a4a5154a0100a
sleemkevin <sleemkevin@gmail.com>
2024-06-22 16:08:31 -0400

    Add files via upload
    AlphaP1LAB1

---
+++ AlphaP1LAB1_SleemKevin.py
@@ -0,0 +1,16 @@
+#Kevin Sleem
+#June 18, 2024
+#P1LAB1
+'''
+This lab includes input and print functions, where the user enters their
+first and last name and gets a greeting.
+'''
+
+print('Enter your first name:', end=' ')
+first_name = input()
+
+print('Enter your last name:', end=' ')
+last_name = input()
+
+print('Hello,', first_name, last_name, '! Welcome to CTI-110')
+
